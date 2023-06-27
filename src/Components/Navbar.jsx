a53c9c954ea6d3f40e578a3a7db087ae42a08829
raashkobby <rashidofori11@gmail.com>
2023-06-27 11:36:10 +0000

    k

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   const navLinks = [
     { name: 'Blogs', path: '/' },
-    { name: 'Create Blog post', path: 'addnew' },
+    { name: 'Create Blog', path: 'addnew' },
   ];
 
   return (
