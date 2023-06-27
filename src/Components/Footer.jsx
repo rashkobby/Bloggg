06955804152ea80e6e257e4ca163ec72cd2de391
raashkobby <rashidofori11@gmail.com>
2023-06-27 14:29:47 +0000

    last one

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer
             className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left fixed inset-x-0 bottom-0 z-50">
             <div className="p-4 text-center font-medium text-neutral-700 dark:text-neutral-200">
-                © 2023 Copyright:   <Link to="#">BLOGGG</Link>
+                © 2023 Copyright:   <Link to="/">BLOGGG</Link>
             </div>
         </footer>
     );
